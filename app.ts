8d4c47d89532ff7a4803a2a46ccacb599443eea0
Abdelmuttalib <hagar9886@gmail.com>
2021-07-04 08:05:08 -0400

    Last commit

--- app.ts
+++ app.ts
@@ -1,1 +1,36 @@
 console.log("TYPESCRIPT");
+
+let foo: number[];
+
+foo = [3, 22];
+
+
+let aTup:[String, Number, Boolean] = ["S", 22, true];
+
+
+let person = {
+    name: 'Ahmed',
+    age: 22
+};
+
+
+function calc(a:number, b:string):void{
+    console.log("E: ", b, a + 2);
+};
+
+const calca = (d:Number, s:Number) => {
+    console.log("Typescript Functions")
+};
+
+
+interface PersonInterface {
+    name: String,
+    age: Number,
+    goo: Boolean
+};
+
+let boo: PersonInterface = {
+    name: "OMAR",
+    age: 22,
+    goo: false
+}
